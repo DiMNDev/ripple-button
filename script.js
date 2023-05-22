d4285c30372400de41296680fcc26e119618fb39
DiMNDev <Dimndev@gmail.com>
2023-05-22 13:01:37 -0600

    new branch

---
+++ script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".btn");
+
+let ripple;
+
+btn.addEventListener("mouseenter", (e) => {
+  console.log(e.target.getBoundingClientRect());
+
+  const left = e.clientX - e.target.getBoundingClientRect().left;
+  console.log(left);
+
+  const top = e.clientY - e.target.getBoundingClientRect().top;
+  console.log(top);
+
+  ripple = document.createElement("div");
+  ripple.classList.add("ripple");
+  ripple.style.left = `${left}px`;
+  ripple.style.top = `${top}px`;
+  btn.prepend(ripple);
+});
